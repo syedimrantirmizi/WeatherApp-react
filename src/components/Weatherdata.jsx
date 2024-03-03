399e94f6d15a318a8f859930ca05a423f8352245
syedimrantirmizi <syedimrantirmizi@gmail.com>
2024-03-03 18:10:24 +0500

    forecast responsive complete

--- src/components/Weatherdata.jsx
+++ src/components/Weatherdata.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Weatherdata = ({weatherData}) => {
     return (
-        <aside className="bg-white/60 rounded-3xl p-10 w-[60%]  h-full flex flex-col justify-between ">
-            <section className='flex w-[90%] justify-between items-center'>
-                <div className="flex justify-between w-[80%] ">
-                    <div><img src={`http://openweathermap.org/img/w/${weatherData?.weather[0].icon}.png`} alt="" width={100} /></div>
+        <aside className="bg-white/60 rounded-3xl p-10 max-sm:p-5 w-[60%]  h-full flex flex-col justify-between max-sm:items-center">
+            <section className='flex w-[90%] justify-between items-center max-sm:flex-col'>
+                <div className="flex justify-between">
+                    <div><img src={`http://openweathermap.org/img/w/${weatherData?.weather[0].icon}.png`} alt="" width={100} height={100} /></div>
                 </div>
-                <div className='font-montserrat-thin text-4xl max-md:text-2xl max-sm:text-xs'>
+                <div className='font-montserrat-thin text-4xl max-md:text-2xl max-sm:text-xl'>
                     {weatherData?.main.temp}°C
                 </div>
             </section>
